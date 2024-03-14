f43105c451caa3b3100b3ead88df75b7e7eb864f
Nithin Suresh <webdevnithin@gmail.com>
2024-03-14 21:05:14 +0530

    Updated: cors

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: `http://localhost:5173`,
+    origin: `https://pdf-manager-be-1.onrender.com`,
     credentials: true,
 }))
 app.use("/uploads",express.static("uploads"))
